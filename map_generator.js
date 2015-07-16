9584b30875cc166659826b29dc3bb2f22969a354
globeriz <globeriz@users.noreply.github.com>
2015-07-16 12:16:14 +0800

    Update map_generator.js

--- map_generator.js
+++ map_generator.js
@@ -5,7 +5,7 @@ var mapgen = function(map_name, map_data)
   var o = $("#map_"+map_name);
   var a = map_data.split('$');
   for (x of a)
-    o.append("<img src='"+mapico_path+x+"' />");
+    o.append("<img src='"+mapico_path+x+".png' />");
 };
 $(document).ready(function(){
 	mapgen("m1","w3$w34$w34$w3$f3$f2$w3$w1$w34$w3$w34$w3$w34$i7$m32$f5$w3$w1$h2$w1$w34$w34$w34$w34$f6$w1$f3$f3$w3$f3$f3$f6$w3$w3$w34$w3$w34$f3$f3$f3$f3$w3$f3$w3$w1$w34$w1$w3$w34$f3$f6$w3$w3$w34$w34$w3$w34$w3$f6$w34$f3$f3$w34$w34$w34$w1$w34$w34$f3$f6$w3$d4$w34$w34$w1$w34$w3$w34$w1$w34$f3$f6$f3$w3$w34$w34$w34$w34$w34$w34$w3$f3$f3$f3$w34$w34$w34$w34$w34$w34$w34$w34$w3$w34$f5$w34$w34$w34$w34$w34$w3$w34$w1$w34$w3$f3$w3$w34$w34$w34$w34$");
