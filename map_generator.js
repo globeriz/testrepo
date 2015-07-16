85e43dd1b2b3fc41d7a24c228edc26ffa3fa7638
globeriz <globeriz@users.noreply.github.com>
2015-07-16 11:49:01 +0800

    Update map_generator.js

--- map_generator.js
+++ map_generator.js
@@ -7,3 +7,6 @@ mapgen = function(map_name, map_data)
   for (x of a)
     o.append("<img src='"+mapico_path+x+"'/">);
 };
+$(document).ready(function(){
+	mapgen("m1","w3$w34$w34$w3$f3$f2$w3$w1$w34$w3$w34$w3$w34$i7$m32$f5$w3$w1$h2$w1$w34$w34$w34$w34$f6$w1$f3$f3$w3$f3$f3$f6$w3$w3$w34$w3$w34$f3$f3$f3$f3$w3$f3$w3$w1$w34$w1$w3$w34$f3$f6$w3$w3$w34$w34$w3$w34$w3$f6$w34$f3$f3$w34$w34$w34$w1$w34$w34$f3$f6$w3$d4$w34$w34$w1$w34$w3$w34$w1$w34$f3$f6$f3$w3$w34$w34$w34$w34$w34$w34$w3$f3$f3$f3$w34$w34$w34$w34$w34$w34$w34$w34$w3$w34$f5$w34$w34$w34$w34$w34$w3$w34$w1$w34$w3$f3$w3$w34$w34$w34$w34$");
+});
