3b83aa2c16d5b2a1cbed542b6fe9142b7bf69265
globeriz <globeriz@users.noreply.github.com>
2015-07-16 09:53:24 +0800

    Update mting.js

--- mting.js
+++ mting.js
@@ -32,11 +32,14 @@ var loadMenu2 = function()
 
 var loadMenu = function()
 {
-	flashObj.after("<div id='hack_menu'></div>");
+	var jf = $(flashObj);
+	jf.after("<div id='hack_menu'></div>");
 	var hm = $("hack_menu");
+	hm.css({"position": "absolute", "top":0, "left", "0"});
 	hm.html("<table><tbody>");
 	var a = ["_root.machar.myhp", String(fR("_root.machar.myhp"))];
 	hm.append( RowGen(a) );
+	hm.append("</tbody></table>");
 }
 
 var btnGenerator = function(f, t)
