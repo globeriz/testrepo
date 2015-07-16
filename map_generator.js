1248a123997e6caee1f904346c4904eb4ff17175
globeriz <globeriz@users.noreply.github.com>
2015-07-16 12:50:18 +0800

    Update map_generator.js

--- map_generator.js
+++ map_generator.js
@@ -1,5 +1,6 @@
 var mapgen = function(map_name, map_data, map_width)
 {
+	map_width = typeof map_width !== 'undefined' ? map_width : 16;
 	mapico_path = './mapico/';
 	$("body").after("<div id='map_"+map_name+"'></div>");
 	var o = $("#map_"+map_name);
@@ -11,12 +12,11 @@ var mapgen = function(map_name, map_data, map_width)
 		   this.onerror = null;
 		   this.src = this.src.split('.').pop()+".bmp";
 		}
-		image.src = mapico_path+x[i]+".png";
+		image.src = mapico_path+a[i]+".png";
 		o.append(image);
-		if (i % map_width == 0) o.append("<br />");
+		if (i % parseInt(map_width) == 0) o.append("<br />");
 	}
 };
 $(document).ready(function(){
-	mapgen("m1","w3$w34$w34$w3$f3$f2$w3$w1$w34$w3$w34$w3$w34$i7$m32$f5$w3$w1$h2$w1$w34$w34$w34$w34$f6$w1$f3$f3$w3$f3$f3$f6$w3$w3$w34$w3$w34$f3$f3$f3$f3$w3$f3$w3$w1$w34$w1$w3$w34$f3$f6$w3$w3$w34$w34$w3$w34$w3$f6$w34$f3$f3$w34$w34$w34$w1$w34$w34$f3$f6$w3$d4$w34$w34$w1$w34$w3$w34$w1$w34$f3$f6$f3$w3$w34$w34$w34$w34$w34$w34$w3$f3$f3$f3$w34$w34$w34$w34$w34$w34$w34$w34$w3$w34$f5$w34$w34$w34$w34$w34$w3$w34$w1$w34$w3$f3$w3$w34$w34$w34$w34$",
-	16);
+	mapgen("m1","w3$w34$w34$w3$f3$f2$w3$w1$w34$w3$w34$w3$w34$i7$m32$f5$w3$w1$h2$w1$w34$w34$w34$w34$f6$w1$f3$f3$w3$f3$f3$f6$w3$w3$w34$w3$w34$f3$f3$f3$f3$w3$f3$w3$w1$w34$w1$w3$w34$f3$f6$w3$w3$w34$w34$w3$w34$w3$f6$w34$f3$f3$w34$w34$w34$w1$w34$w34$f3$f6$w3$d4$w34$w34$w1$w34$w3$w34$w1$w34$f3$f6$f3$w3$w34$w34$w34$w34$w34$w34$w3$f3$f3$f3$w34$w34$w34$w34$w34$w34$w34$w34$w3$w34$f5$w34$w34$w34$w34$w34$w3$w34$w1$w34$w3$f3$w3$w34$w34$w34$w34$");
 });
