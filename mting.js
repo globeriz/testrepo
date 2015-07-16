fde63eba8c3b02d5e9cb303bca050476dec6d9a2
globeriz <globeriz@users.noreply.github.com>
2015-07-16 10:01:25 +0800

    Update mting.js

--- mting.js
+++ mting.js
@@ -37,7 +37,7 @@ var loadMenu = function()
 	hm.css({"position": "absolute", "top":0, "left":0});
 	hm.html("<table><tbody>");
 	var a = ["_root.machar.myhp", String(fR("_root.machar.myhp"))];
-	hm.append( RowGen(a) );
+	hm.append( rowGen(a) );
 	hm.append("</tbody></table>");
 	hm.load();
 }
@@ -46,10 +46,10 @@ var btnGenerator = function(f, t)
 {
 	return("<button type='button' onclick='"+f+"'>"+t+"</button>");
 };
-var RowGen = function(a)
+var rowGen = function(a)
 {
-	if (typeof a !== "array")
-		return "";
+	if (!Array.isArray(a))
+		return ("<tr><td>"+a+"</td></tr>");
 	var s = "<tr>";
 	for (var x in a)
 		s += ("<td>"+x+"</td>");
