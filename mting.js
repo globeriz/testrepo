d1c968f47c44e4fbf26b35fc217385471df45b5e
globeriz <globeriz@users.noreply.github.com>
2015-07-16 09:58:35 +0800

    Update mting.js

--- mting.js
+++ mting.js
@@ -33,7 +33,7 @@ var loadMenu2 = function()
 var loadMenu = function()
 {
 	$("body").after("<div id='hack_menu'></div>");
-	var hm = $("hack_menu");
+	var hm = $("#hack_menu");
 	hm.css({"position": "absolute", "top":0, "left":0});
 	hm.html("<table><tbody>");
 	var a = ["_root.machar.myhp", String(fR("_root.machar.myhp"))];
